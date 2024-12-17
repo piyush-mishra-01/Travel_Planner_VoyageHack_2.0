5757f39e67a862752221635619ec22d03fec7a0e
Piyush Mishra <vbs-tms@vacobinary.in>
2024-12-17 21:59:53 +0530

    BudgetManagement service commit

---
+++ backend/budgetManagementService/budgetManagementService/src/main/java/com/voyage/budgetManagementService/BudgetManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.voyage.budgetManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementServiceApplication.class, args);
+	}
+
+}
