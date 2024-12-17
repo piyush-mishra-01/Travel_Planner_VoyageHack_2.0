9876045f27a9e3f443ede934f0bd218f45ea4f9f
Piyush Mishra <vbs-tms@vacobinary.in>
2024-12-17 21:56:20 +0530

    Itenary planner service commit

---
+++ backend/ItenaryManagementService/src/main/java/com/voyage/ItenaryManagementService/ItenaryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.voyage.ItenaryManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItenaryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItenaryManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/ItenaryManagementService/src/test/java/com/voyage/ItenaryManagementService/ItenaryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyage.ItenaryManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItenaryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
